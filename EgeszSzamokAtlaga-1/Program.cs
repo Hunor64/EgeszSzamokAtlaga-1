e6b6f34cd69d47cf9c6ac6c43a0c48710f06b4f6
hunor64 <nhunor06@gmail.com>
2024-09-05 13:35:48 +0200

    Add project files.

---
+++ EgeszSzamokAtlaga-1/Program.cs
@@ -0,0 +1,28 @@
+﻿List<int> szamok = new();
+while (true)
+{
+	string szoveg = Console.ReadLine();
+	if (szoveg != "")
+	{
+		try
+		{
+			szamok.Add(Convert.ToInt32(szoveg));
+		}
+		catch (Exception)
+		{
+
+		}
+	}
+	else
+	{
+		break;
+	}
+}
+int osszes = 0;
+foreach (var item in szamok)
+{
+	osszes = osszes + item;
+}
+Console.WriteLine(osszes);
+Console.WriteLine(szamok.Count());
+Console.WriteLine(Math.Round(osszes*1.0/szamok.Count(),2));
